9b33ccdc63946cab2abfbbbab3314ce923f9ebd8
Atmos <zonder2009@gmail.com>
2026-06-14 23:25:16 +0300

    opt. structure app

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,14 +1,13 @@
 import DataDriftTable from '../DataDriftTable/DataDriftTable';
 import ManufacturersPage from '../pages/ManufacturersPage';
 import './App.scss';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <>
-      {/* <h2>FH 6 - Drift Results Table </h2> */}
-      {/* <DataDriftTable /> */}
+    <Container>
       <ManufacturersPage />
-    </>
+    </Container>
   );
 }
 
